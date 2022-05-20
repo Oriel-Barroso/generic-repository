037cdf28454ad4c9b4b9f468ce302f04cd6c7390
Francisco Del Roio <francipvb@hotmail.com>
2022-05-20 08:39:17 -0300

    Added `MappedRepository` and `CacheRepository`.

--- generic_repository/__init__.py
+++ generic_repository/__init__.py
@@ -1,6 +1,8 @@
 # flake8: noqa F401
 
 from .base import GenericBaseRepository
+from .cached import CacheRepository
+from .composition import MappedRepository
 from .exceptions import CrudException, InvalidPayloadException, ItemNotFoundException
 from .mapper import ConstructorMapper, LambdaMapper, Mapper, ToFunctionArgsMapper
 
